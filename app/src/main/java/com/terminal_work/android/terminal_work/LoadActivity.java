407d864f31bf89c0751f8949502b9bd77dd5fc33
shuiliudeng <2947403813@qq.com>
2018-12-18 00:01:57 +0800

    update1.0.2

--- app/src/main/java/com/terminal_work/android/terminal_work/LoadActivity.java
+++ app/src/main/java/com/terminal_work/android/terminal_work/LoadActivity.java
@@ -1,13 +1,29 @@
 package com.terminal_work.android.terminal_work;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoadActivity extends AppCompatActivity {
+
+    private Button mloadButton;
+    private Button mregisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.load);
+
+        mloadButton=(Button)findViewById(R.id.loadbutton);
+        mregisterButton=(Button)findViewById(R.id.registerbutton);
+        mregisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoadActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/terminal_work/android/terminal_work/RegisterActivity.java
+++ app/src/main/java/com/terminal_work/android/terminal_work/RegisterActivity.java
@@ -1,13 +1,26 @@
 package com.terminal_work.android.terminal_work;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Button mregisterButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        mregisterButton1=(Button)findViewById(R.id.registerbutton1);
+        mregisterButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterActivity.this,DailyBillActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
